509c117d3c7801a1988968c565af71429f57285d
kwonih1020 <kwonih1020@gmail.com>
2022-08-31 15:10:28 +0900

    #1 kwon feat remove calendar from detail page and will add to header section

--- tiger-fe/src/components/vehicle_detail/Reservation.jsx
+++ tiger-fe/src/components/vehicle_detail/Reservation.jsx
@@ -12,92 +12,82 @@ import styled from "styled-components";
 import Modal from "../../global_elements/Modal";
 
 const Reservation = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
-    <button className="example-custom-input" onClick={onClick} ref={ref}>
-      {value}
-    </button>
-  ));
+  // let user to pick startDate and endDate
+  // const [startDate, setStartDate] = useState(new Date());
+  // const [endDate, setEndDate] = useState(new Date());
 
-  const disabledDates = ["2022-09-15", "2022-09-20"];
+  // // date picker button custom
+  // const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
+  //   <button className="example-custom-input" onClick={onClick} ref={ref}>
+  //     {value}
+  //   </button>
+  // ));
 
-  const disabledDate = (current) => {
-    return (
-      moment(current).day() === 0 ||
-      disabledDates.find(
-        (date) => date === moment(current).format("YYYY-MM-DD")
-      )
-    );
-  };
+  // // modal open or close
+  // const [isModalOpen, setIsModalOpen] = useState(false);
+  // const showModal = () => {
+  //   setIsModalOpen(!isModalOpen);
+  // };
 
-  const [IsModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(!IsModalOpen);
-  };
+  // console.log("start-date", startDate);
+  // console.log("end-date", endDate);
+  return <div></div>;
+  // <CalendarContainer>
+  //   <CalendarWrapper>
+  //     <NewDatePicker
+  //       selected={startDate}
+  //       onChange={(date) => setStartDate(date)}
+  //       selectsStart
+  //       startDate={startDate}
+  //       endDate={endDate}
+  //       locale={ko}
+  //       dateFormat="yyyy-MM-dd"
+  //       minDate={new Date()}
+  //       customInput={<ExampleCustomInput />}
+  //       shouldCloseOnSelect={false}
+  //     />
+  //   </CalendarWrapper>
+  //   <CalendarWrapper>
+  //     <NewDatePicker
+  //       selected={endDate}
+  //       onChange={(date) => setEndDate(date)}
+  //       selectsEnd
+  //       startDate={startDate}
+  //       endDate={endDate}
+  //       minDate={startDate}
+  //       locale={ko}
+  //       dateFormat="yyyy-MM-dd"
+  //       customInput={<ExampleCustomInput />}
+  //       shouldCloseOnSelect={false}
+  //     />
+  //     <button onClick={showModal}>결제하기</button>
 
-  console.log("start-date", startDate);
-  console.log("end-date", endDate);
-  return (
-    <CalendarContainer>
-      <CalendarWrapper>
-        <NewDatePicker
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          selectsStart
-          startDate={startDate}
-          endDate={endDate}
-          locale={ko}
-          dateFormat="yyyy-MM-dd"
-          minDate={new Date()}
-          // isClearable
-          customInput={<ExampleCustomInput />}
-          disabledDate={disabledDate}
-        />
-      </CalendarWrapper>
-      <CalendarWrapper>
-        <NewDatePicker
-          selected={endDate}
-          onChange={(date) => setEndDate(date)}
-          selectsEnd
-          startDate={startDate}
-          endDate={endDate}
-          minDate={startDate}
-          locale={ko}
-          dateFormat="yyyy-MM-dd"
-          // isClearable
-          customInput={<ExampleCustomInput />}
-          disabledDate={disabledDate}
-        />
-        <button onClick={showModal}>결제하기</button>
-        {IsModalOpen ? (
-          <Modal showModal={showModal}>
-            <p>{String(startDate.toISOString().slice(0, 10))}</p>
-            <p>{String(endDate.toISOString().slice(0, 10))}</p>
-          </Modal>
-        ) : null}
-      </CalendarWrapper>
-    </CalendarContainer>
-  );
+  //     {/* modal open to payment modal */}
+  //     {isModalOpen ? (
+  //       <Modal showModal={showModal}>
+  //         <p>{String(startDate.toISOString().slice(0, 10))}</p>
+  //         <p>{String(endDate.toISOString().slice(0, 10))}</p>
+  //       </Modal>
+  //     ) : null}
+  //   </CalendarWrapper>
+  // </CalendarContainer>
 };
 
-const CalendarContainer = styled.div`
-  display: flex;
-`;
+// const CalendarContainer = styled.div``;
 
-const CalendarWrapper = styled.div`
-  width: 50%;
-  margin: 0 auto;
-`;
+// const CalendarWrapper = styled.div`
+//   width: 50%;
+//   margin: 0 auto;
+// `;
 
-const NewDatePicker = styled(DatePicker)`
-  width: 130px;
-  height: 30px;
-  box-sizing: border-box;
-  padding: 8px 20px;
-  border-radius: 4px;
-  border: 1px solid orange;
-  font-size: 12px;
-`;
+// const NewDatePicker = styled(DatePicker)`
+//   width: 130px;
+//   height: 30px;
+//   box-sizing: border-box;
+//   padding: 8px 20px;
+//   border-radius: 4px;
+//   border: 1px solid orange;
+//   font-size: 12px;
+// `;
 
 export default Reservation;
