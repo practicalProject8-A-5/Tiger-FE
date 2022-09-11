5c0a4099398c98883fc31535d1d09fc1e333477b
kwonih1020 <kwonih1020@gmail.com>
2022-09-11 15:29:17 +0900

    nth

--- tiger-fe/src/components/vehicle_detail/KakaoMapDetail.jsx
+++ tiger-fe/src/components/vehicle_detail/KakaoMapDetail.jsx
@@ -49,15 +49,6 @@ const KakaoMapDetail = () => {
         map.setCenter(coords);
       }
     });
-    // const map = new kakao.maps.Map(container, options);
-    // const markerPosition = new kakao.maps.LatLng(
-    //   37.365264512305174,
-    //   127.10676860117488
-    // );
-    // const marker = new kakao.maps.Marker({
-    //   position: markerPosition,
-    // });
-    // marker.setMap(map);
   }, [vehicleDetailsLocation]);
 
   return (
