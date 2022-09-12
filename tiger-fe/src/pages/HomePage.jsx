b9bc7d44e8b0a485cdc802dd9cf48ac09c029246
kwonih1020 <kwonih1020@gmail.com>
2022-09-12 12:04:55 +0900

    feat added intro page for init setup

--- tiger-fe/src/pages/HomePage.jsx
+++ tiger-fe/src/pages/HomePage.jsx
@@ -1,9 +1,21 @@
 // eslint-disable-next-line
 
 import React from "react";
+import Header from "../global_elements/Header";
+import Search from "../global_elements/Search";
+import GlobalLayout from "../global/GlobalLayout";
+import Desc from "../components/main/Desc";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Search />
+      <GlobalLayout>
+        <Desc />
+      </GlobalLayout>
+    </>
+  );
 };
 
 export default HomePage;
