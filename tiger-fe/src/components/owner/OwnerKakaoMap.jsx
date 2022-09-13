856d0a41071196c4f66d9fcc335aa685caad6b61
kwonih1020 <kwonih1020@gmail.com>
2022-09-13 10:49:15 +0900

    #53 bug fixed get request when first rendering page

--- tiger-fe/src/components/owner/OwnerKakaoMap.jsx
+++ tiger-fe/src/components/owner/OwnerKakaoMap.jsx
@@ -12,6 +12,8 @@ const OwnerKakaoMap = ({ address, locationObj, setLocationObj }) => {
   // console.log(locationInfo);
 
   const locationOnMap = address;
+  console.log(locationOnMap);
+  console.log(locationObj);
 
   // useEffect(() => {
   //   const mapContainer = document.getElementById("map");
@@ -27,28 +29,28 @@ const OwnerKakaoMap = ({ address, locationObj, setLocationObj }) => {
 
   //   // 주소로 좌표를 검색합니다
   //   // 이거 채운이가 필요
-  //   geocoder.addressSearch(locationOnMap, function (result, status) {
-  //     // 정상적으로 검색이 완료됐으면
-  //     if (status === kakao.maps.services.Status.OK) {
-  //       var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+  // geocoder.addressSearch(locationOnMap, function (result, status) {
+  //   // 정상적으로 검색이 완료됐으면
+  //   if (status === kakao.maps.services.Status.OK) {
+  //     var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-  //       // 결과값으로 받은 위치를 마커로 표시합니다
-  //       var marker = new kakao.maps.Marker({
-  //         map: map,
-  //         position: coords,
-  //       });
+  //     // 결과값으로 받은 위치를 마커로 표시합니다
+  //     var marker = new kakao.maps.Marker({
+  //       map: map,
+  //       position: coords,
+  //     });
 
-  //       // 인포윈도우로 장소에 대한 설명을 표시합니다
-  //       var infowindow = new kakao.maps.InfoWindow({
-  //         content:
-  //           '<div style="width:150px;text-align:center;padding:6px 0;">차량 위치</div>',
-  //       });
-  //       infowindow.open(map, marker);
+  //     // 인포윈도우로 장소에 대한 설명을 표시합니다
+  //     var infowindow = new kakao.maps.InfoWindow({
+  //       content:
+  //         '<div style="width:150px;text-align:center;padding:6px 0;">차량 위치</div>',
+  //     });
+  //     infowindow.open(map, marker);
 
-  //       // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-  //       map.setCenter(coords);
-  //     }
-  //   });
+  //     // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+  //     map.setCenter(coords);
+  //   }
+  // });
   // }, [locationOnMap]);
 
   // 익현 --------------------------------------------------
@@ -85,34 +87,38 @@ const OwnerKakaoMap = ({ address, locationObj, setLocationObj }) => {
     // 지도를 생성합니다
     var map = new kakao.maps.Map(mapContainer, mapOption);
 
+    console.log(map);
+
     // const resultX = locationObj.locationX;
     // const resultY = locationObj.locationY;
 
-    var coords = new kakao.maps.LatLng(
-      locationObj.locationY,
-      locationObj.locationX
-    );
+    if (locationOnMap) {
+      var coords = new kakao.maps.LatLng(
+        locationObj.locationY,
+        locationObj.locationX
+      );
 
-    var marker = new kakao.maps.Marker({
-      map: map,
-      position: coords,
-    });
+      var marker = new kakao.maps.Marker({
+        map: map,
+        position: coords,
+      });
 
-    // 인포윈도우로 장소에 대한 설명을 표시합니다
-    var infowindow = new kakao.maps.InfoWindow({
-      content:
-        '<div style="width:150px;text-align:center;padding:6px 0;">차량 위치</div>',
-    });
-    infowindow.open(map, marker);
+      // 인포윈도우로 장소에 대한 설명을 표시합니다
+      var infowindow = new kakao.maps.InfoWindow({
+        content:
+          '<div style="width:150px;text-align:center;padding:6px 0;">차량 위치</div>',
+      });
+      infowindow.open(map, marker);
 
-    // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-    map.setCenter(coords);
+      // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+      map.setCenter(coords);
 
-    console.log(coords);
+      console.log(coords);
+    }
   };
 
   useEffect(() => {
-    if (locationOnMap !== undefined) getCoords(locationOnMap);
+    if (locationOnMap) getCoords(locationOnMap);
   }, [locationOnMap]);
 
   // 맵을 생성하는 함수를 처음 렌더링 그리고 locationObj가 변경될 때만 랜더링 하고
@@ -123,7 +129,7 @@ const OwnerKakaoMap = ({ address, locationObj, setLocationObj }) => {
   const locationX = locationObj.locationX;
   const locationY = locationObj.locationY;
   // 위도경도 값
-  // console.log(locationX, locationY);
+  console.log(locationX, locationY);
 
   // --------------익현 끝 --------------------------------------
 
