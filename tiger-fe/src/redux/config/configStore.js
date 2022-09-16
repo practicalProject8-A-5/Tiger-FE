143223f61c958fc65b2e0860c2b5e47581fd0870
kwonih1020 <kwonih1020@gmail.com>
2022-09-17 01:50:32 +0900

    turned on persist

--- tiger-fe/src/redux/config/configStore.js
+++ tiger-fe/src/redux/config/configStore.js
@@ -24,16 +24,16 @@ const reducer = combineReducers({
   getDateListSlice,
 });
 
-// const persistConfig = {
-//   key: "root",
-//   storage,
-// };
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-// const persistedReducer = persistReducer(persistConfig, reducer);
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  // reducer: persistedReducer,
-  reducer,
+  reducer: persistedReducer,
+  // reducer,
   // middleware: (getDefaultMiddleware) =>
   //   getDefaultMiddleware({ serializableCheck: false }).concat(logger),
   // devTools: process.env.NODE_ENV !== "production",
@@ -43,5 +43,5 @@ export const store = configureStore({
   devTools: process.env.NODE_ENV !== "production",
 });
 
-// export const persistor = persistStore(store);
-export default store;
+export const persistor = persistStore(store);
+// export default store;
