517577810b448d4b41af6efd16735b78b173ba95
kwonih1020 <kwonih1020@gmail.com>
2022-09-29 22:10:28 +0900

    #123 added google analytic

--- tiger-fe/src/index.js
+++ tiger-fe/src/index.js
@@ -4,13 +4,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import ReactGA from "react-ga";
+import ReactGA from "react-ga";
 import { persistor, store } from "./redux/config/configStore";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
-// const Google_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
-// ReactGA.initialize(Google_ID);
+const Google_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
+ReactGA.initialize(Google_ID);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
