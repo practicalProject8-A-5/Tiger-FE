9f1a908161943878d23c5376269bbe98d25749df
kwonih1020 <kwonih1020@gmail.com>
2022-09-29 22:03:36 +0900

    #123 bug added google analytics

--- tiger-fe/src/App.js
+++ tiger-fe/src/App.js
@@ -3,7 +3,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import RouteChangeTracker from "./redux/shared/RouteChangeTracker";
 import GlobalRouter from "./global/GlobalRouter";
 import { loader } from "./redux/modules/memberSlice";
 import "./App.css";
@@ -13,8 +12,6 @@ function App() {
   useEffect(() => {
     dispatch(loader());
   }, [dispatch]);
-
-  RouteChangeTracker();
 
   return (
     <BrowserRouter>
