3915bb62518c560b4ce9697fec955819ac035631
kwonih1020 <kwonih1020@gmail.com>
2022-09-29 22:05:28 +0900

    pull from ick

--- tiger-fe/src/App.js
+++ tiger-fe/src/App.js
@@ -1,20 +1,38 @@
 // eslint-disable-next-line
 
 import React, { useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import RouteChangeTracker from "./redux/shared/RouteChangeTracker";
 import GlobalRouter from "./global/GlobalRouter";
 import { loader } from "./redux/modules/memberSlice";
 import "./App.css";
+// import ReactGA from "react-ga";
+
+// ReactGA.event({
+//   category: "User",
+//   action: "Created an Account",
+// });
+// ReactGA.exception({
+//   description: "An error ocurred",
+//   fatal: true,
+// });
 
 function App() {
   const dispatch = useDispatch();
+  // const locations = useLocation();
+
   useEffect(() => {
     dispatch(loader());
   }, [dispatch]);
 
-  RouteChangeTracker();
+  // useEffect(() => {
+  //   ReactGA.initialize("user id");
+  //   locations.listen((location) => {
+  //     ReactGA.set({ page: location.pathname }); // Update the user's current page
+  //     ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  //   });
+  //   // ReactGA.pageview(window.location.pathname + window.location.search);
+  // }, []);
 
   return (
     <BrowserRouter>
